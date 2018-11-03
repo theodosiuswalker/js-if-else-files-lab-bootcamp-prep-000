2d5a82414640de776b1a2f142be50d6fabee1548
Theodosius Walker <theodosiuswalker@gmail.com>
2018-11-03 15:46:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var name ="Susan"
+var name = "Joe"
 var height = "74"
-if (name === "susan") {   
-  console.log("The name is correct")
-}
-// Don't worry about this
-module.exports = { name, height
-}
 
